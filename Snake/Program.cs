70cd46f36d9d1fcb058fbfeee1ee3ee798aaebfa
Andreas010 <winjas@gmx.net>
2022-03-03 21:31:55 +0100

    Added CMD arguments --help --speed [value]
    Made it impossible to go backwards into yourself

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -7,16 +7,46 @@ namespace Snake
     {
         static readonly int width = Console.WindowWidth;
         static readonly int height = Console.WindowHeight;
+        static int reactionTime = 50;
 
-        static void Main()
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i].ToLower();
+
+                if (arg == "--help")
+                {
+                    Console.Write(
+                        "SNAKE IN A CONSOLE\n" +
+                        "------------------\n" +
+                        "--help => This text\n" +
+                        "--speed [value] => change the time waited between each step in ms (def=50)\n"
+                        );
+                    return;
+                }
+                else if (arg == "--speed" && i + 1 < args.Length)
+                {
+                    if (int.TryParse(args[i + 1], out int _reactionTime))
+                        reactionTime = _reactionTime;
+                }
+            }
+
+            while (true)
+            {
+                Game();
+            }          
+        }
+
+        static void Game()
         {
             Console.Clear();
-            sbyte[,] stepMatrix = new sbyte[width, height];
+            byte[,] stepMatrix = new byte[width, height];
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    stepMatrix[i, j] = -1;
+                    stepMatrix[i, j] = 0xFF;
                 }
             }
 
@@ -27,8 +57,8 @@ namespace Snake
             int tailX = headX;
             int tailY = headY + 1;
             stepMatrix[tailX, tailY] = 0;
-            sbyte direction = 0;
-            sbyte dir = 0;
+            byte direction = 0;
+            byte dir = 0;
 
             int foodX = headX;
             int foodY = headY - 1;
@@ -37,8 +67,6 @@ namespace Snake
             Console.CursorVisible = false;
 
             Random r = new();
-
-            int reactionTime = 50;
 
             while (true)
             {
@@ -54,14 +82,14 @@ namespace Snake
                 {
                     Console.SetCursorPosition(foodX, foodY);
                     Console.Write('█');
-                    foodX = r.Next(0, width-1);
-                    foodY = r.Next(0, height-1);
+                    foodX = r.Next(0, width - 1);
+                    foodY = r.Next(0, height - 1);
                     score++;
                 }
                 else
                 {
                     dir = stepMatrix[tailX, tailY];
-                    stepMatrix[tailX, tailY] = -1;
+                    stepMatrix[tailX, tailY] = 0xFF;
                     if (dir == 0)
                         tailY--;
                     else if (dir == 1)
@@ -78,13 +106,13 @@ namespace Snake
                 if (Console.KeyAvailable)
                 {
                     ConsoleKey key = Console.ReadKey(true).Key;
-                    if (key == ConsoleKey.UpArrow)
+                    if (key == ConsoleKey.UpArrow && direction != 2)
                         direction = 0;
-                    else if (key == ConsoleKey.RightArrow)
+                    else if (key == ConsoleKey.RightArrow && direction != 3)
                         direction = 1;
-                    else if (key == ConsoleKey.DownArrow)
+                    else if (key == ConsoleKey.DownArrow && direction != 0)
                         direction = 2;
-                    else if (key == ConsoleKey.LeftArrow)
+                    else if (key == ConsoleKey.LeftArrow && direction != 1)
                         direction = 3;
                 }
 
@@ -102,18 +130,16 @@ namespace Snake
                 headX = Wrap(headX, width - 1);
                 headY = Wrap(headY, height - 1);
 
-                if(stepMatrix[headX, headY] != -1)
+                if (stepMatrix[headX, headY] != 0xFF)
                 {
-                    // TODO: Death Screen
                     Console.Clear();
                     while (Console.KeyAvailable)
                         Console.ReadKey(true);
                     Console.SetCursorPosition(width / 2 - 5, height / 2);
                     Console.Write("GAME  OVER");
-                    Console.SetCursorPosition(width / 2 - 5, height / 2+2);
+                    Console.SetCursorPosition(width / 2 - 5, height / 2 + 2);
                     Console.Write($"SCORE: {score}");
                     Console.ReadKey(true);
-                    Main();
                 }
 
                 TimeSpan span = DateTime.Now - past;
